3afb18baa52a10bdc081a840925560ff779fb628
csaba-feher-sonarsource <csaba.feher@sonarsource.com>
2023-01-23 10:31:14 +0100

    initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+import java.util.logging.Logger;
+
+public class test {
+
+public int val;
+public boolean TestMethod(int x) {
+
+  int nonused=0;
+  int noused2;
+
+  String password = "ABCD";
+
+  if(x==1) return  true;
+  else return false;
+
+}
+}
